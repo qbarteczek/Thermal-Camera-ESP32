17a8567d4b43bef2edf6d5a91b1f351c5d765bf7
Qbart <165225527+qbarteczek@users.noreply.github.com>
2024-07-09 19:43:40 +0200

    Update thermal_camera.ino

--- thermal_camera.ino
+++ thermal_camera.ino
@@ -15,7 +15,7 @@
 #define AMG88xx_PIXEL_ARRAY_SIZE 64
 
 // Create an instance of the Adafruit ILI9341 TFT library
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_CLK, TFT_MISO);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
 // Create an instance of the Adafruit AMG88xx IR sensor library
 Adafruit_AMG88xx amg;
@@ -25,54 +25,59 @@ uint16_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Setup started");
 
   // Initialize TFT display
   tft.begin();
   tft.setRotation(3);  // Adjust rotation if needed
+  Serial.println("TFT initialized");
 
   // Initialize AMG8833 sensor
   if (!amg.begin()) {
     Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
     while (1);
   }
+  Serial.println("AMG8833 initialized");
 
   // Clear the TFT screen
   tft.fillScreen(ILI9341_BLACK);
   tft.setTextColor(ILI9341_WHITE);
   tft.setTextSize(2);
+  Serial.println("Screen cleared");
 }
 
 void loop() {
   // Read pixel data from AMG8833
   float pixels_raw[AMG88xx_PIXEL_ARRAY_SIZE];
   amg.readPixels(pixels_raw);
+  Serial.println("Pixel data read from AMG8833");
 
   // Interpolate and scale the image to 320x240
   for (int y = 0; y < SCREEN_HEIGHT; y++) {
     for (int x = 0; x < SCREEN_WIDTH; x++) {
-      float x_ratio = x * 8.0 / 320.0;
-      float y_ratio = y * 8.0 / 240.0;
+      float x_ratio = x * 8.0 / SCREEN_WIDTH;
+      float y_ratio = y * 8.0 / SCREEN_HEIGHT;
       int x_int = (int)x_ratio;
       int y_int = (int)y_ratio;
       float x_frac = x_ratio - x_int;
       float y_frac = y_ratio - y_int;
 
       int index_tl = y_int * 8 + x_int;
-      int index_tr = y_int * 8 + (x_int + 1);
-      int index_bl = (y_int + 1) * 8 + x_int;
-      int index_br = (y_int + 1) * 8 + (x_int + 1);
+      int index_tr = y_int * 8 + (x_int < 7 ? x_int + 1 : x_int);
+      int index_bl = (y_int < 7 ? y_int + 1 : y_int) * 8 + x_int;
+      int index_br = (y_int < 7 ? y_int + 1 : y_int) * 8 + (x_int < 7 ? x_int + 1 : x_int);
 
       float interp_val = pixels_raw[index_tl] * (1 - x_frac) * (1 - y_frac) +
                          pixels_raw[index_tr] * x_frac * (1 - y_frac) +
                          pixels_raw[index_bl] * (1 - x_frac) * y_frac +
                          pixels_raw[index_br] * x_frac * y_frac;
 
-      // Map temperature value to color (assuming grayscale for simplicity)
-      uint16_t color = tft.color565(interp_val, interp_val, interp_val);
-
+      // Map temperature value to color
+      uint16_t color = temperatureToColor(interp_val);
       tft.drawPixel(x, y, color);
     }
   }
+  Serial.println("Interpolation and display update complete");
 
   // Display the central temperature value
   float central_temp = pixels_raw[4 * 8 + 4];  // Temperature at the central point (4,4)
@@ -81,6 +86,27 @@ void loop() {
   tft.print("Center Temp: ");
   tft.print(central_temp, 2);
   tft.print(" C");
+  Serial.print("Central Temp: ");
+  Serial.print(central_temp, 2);
+  Serial.println(" C");
 
   delay(100);  // Adjust delay as needed for display rate
+}
+
+uint16_t temperatureToColor(float temp) {
+  uint8_t r, g, b;
+  if (temp < 20) {
+    r = 0;
+    g = 0;
+    b = map(temp, 0, 20, 255, 0);
+  } else if (temp < 30) {
+    r = 0;
+    g = map(temp, 20, 30, 0, 255);
+    b = 0;
+  } else {
+    r = map(temp, 30, 40, 0, 255);
+    g = 255 - r;
+    b = 0;
+  }
+  return tft.color565(r, g, b);
 }
