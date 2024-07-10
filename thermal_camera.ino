94593930b9d5936178adf551ecac95c3b2dad32b
Qbart <165225527+qbarteczek@users.noreply.github.com>
2024-07-11 00:41:50 +0200

    Update thermal_camera.ino

--- thermal_camera.ino
+++ thermal_camera.ino
@@ -1,116 +1,138 @@
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_AMG88xx.h>
 
-#define TFT_CS   14  // Pin CS ekranu TFT podłączony do pinu GPIO 14 (ESP32)
-#define TFT_DC   27  // Pin DC/RD ekranu TFT podłączony do pinu GPIO 27 (ESP32)
-#define TFT_RST  33  // Pin RST ekranu TFT podłączony do pinu GPIO 33 (ESP32)
-#define TFT_MOSI 23  // Pin MOSI SPI ekranu TFT podłączony do pinu GPIO 23 (ESP32)
-#define TFT_CLK  18  // Pin CLK SPI ekranu TFT podłączony do pinu GPIO 18 (ESP32)
-#define TFT_MISO 19  // Pin MISO SPI ekranu TFT podłączony do pinu GPIO 19 (ESP32)
+// Definicje pinów dla wyświetlacza TFT
+#define TFT_CS   4  
+#define TFT_DC   17  
+#define TFT_RST  16  
+#define TFT_MOSI 23  
+#define TFT_SCK  18  
+#define TFT_MISO 19  
 
-#define SCREEN_WIDTH  320
-#define SCREEN_HEIGHT 240
-#define AMG88xx_PIXEL_ARRAY_SIZE 64
-
-// Create an instance of the Adafruit ILI9341 TFT library
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_CLK, TFT_MISO);
-
-// Create an instance of the Adafruit AMG88xx IR sensor library
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 Adafruit_AMG88xx amg;
 
-// Buffer to hold interpolated 320x240 image
-uint16_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
+// Tablice do przechowywania wartości temperatur
+float pixels[64]; // 8x8 matryca
+float interpolatedPixels[40][30]; // 40x30 matryca po interpolacji
+uint16_t colors[40][30]; // Tablica kolorów do wyświetlenia
+
+const int gridWidth = 8; // Szerokość matrycy czujnika
+const int gridHeight = 8; // Wysokość matrycy czujnika
+const int displayWidth = 320;
+const int displayHeight = 240;
+const int cellWidth = displayWidth / 40; // Szerokość komórki po skalowaniu (320 / 40)
+const int cellHeight = displayHeight / 30; // Wysokość komórki po skalowaniu (240 / 30)
+
+// Funkcja do mapowania temperatur na kolory (kolory termowizyjne)
+uint16_t mapTemperatureToColor(float temperature) {
+    // Zdefiniuj zakres temperatur dla kolorów (możesz dostosować do swoich potrzeb)
+    float minTemp = 20.0;
+    float maxTemp = 40.0;
+    
+    if (temperature < minTemp) temperature = minTemp;
+    if (temperature > maxTemp) temperature = maxTemp;
+    
+    float range = maxTemp - minTemp;
+    float normalized = (temperature - minTemp) / range;
+
+    // Kolory termowizyjne: od ciemnoniebieskiego przez jasnoniebieski, żółty do czerwonego
+    uint8_t red, green, blue;
+    
+    if (normalized < 0.25) {
+        red = 0;
+        green = 0;
+        blue = 128 + 127 * (normalized / 0.25);
+    } else if (normalized < 0.5) {
+        red = 0;
+        green = 255 * ((normalized - 0.25) / 0.25);
+        blue = 255;
+    } else if (normalized < 0.75) {
+        red = 255 * ((normalized - 0.5) / 0.25);
+        green = 255;
+        blue = 255 - 255 * ((normalized - 0.5) / 0.25);
+    } else {
+        red = 255;
+        green = 255 - 255 * ((normalized - 0.75) / 0.25);
+        blue = 0;
+    }
+
+    return tft.color565(red, green, blue);
+}
+
+// Funkcja do interpolacji danych 8x8 do 40x30
+void interpolatePixels() {
+    for (int y = 0; y < 30; y++) {
+        for (int x = 0; x < 40; x++) {
+            float gx = (float)x / 40 * 7;
+            float gy = (float)y / 30 * 7;
+            int gxi = (int)gx;
+            int gyi = (int)gy;
+            float c00 = pixels[gyi * 8 + gxi];
+            float c10 = pixels[gyi * 8 + (gxi + 1) % 8];
+            float c01 = pixels[(gyi + 1) % 8 * 8 + gxi];
+            float c11 = pixels[(gyi + 1) % 8 * 8 + (gxi + 1) % 8];
+            float rm = gx - gxi;
+            float cm = gy - gyi;
+            interpolatedPixels[x][y] = c00 * (1 - rm) * (1 - cm) + c10 * rm * (1 - cm) + c01 * (1 - rm) * cm + c11 * rm * cm;
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Setup started");
-
-  // Initialize TFT display
-  if (!tft.begin()) {
-    Serial.println("TFT initialization failed!");
-    while (1);
-  }
-  Serial.println("TFT initialized");
-  tft.setRotation(3);  // Adjust rotation if needed
-  Serial.println("TFT rotation set");
-
-  // Initialize AMG8833 sensor
-  if (!amg.begin()) {
-    Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
-    while (1);
-  }
-  Serial.println("AMG8833 initialized");
-
-  // Clear the TFT screen
-  tft.fillScreen(ILI9341_BLACK);
-  tft.setTextColor(ILI9341_WHITE);
-  tft.setTextSize(2);
-  Serial.println("Screen cleared");
+    Serial.begin(115200);
+    tft.begin();
+    tft.setRotation(1); // Ustawienie orientacji poziomej
+    tft.fillScreen(ILI9341_BLACK);
+    
+    if (!amg.begin()) {
+        Serial.println("Nie można znaleźć czujnika AMG8830");
+        while (1);
+    }
+    delay(100);
 }
 
 void loop() {
-  // Read pixel data from AMG8833
-  float pixels_raw[AMG88xx_PIXEL_ARRAY_SIZE];
-  amg.readPixels(pixels_raw);
-  Serial.println("Pixel data read from AMG8833");
-
-  // Interpolate and scale the image to 320x240
-  for (int y = 0; y < SCREEN_HEIGHT; y++) {
-    for (int x = 0; x < SCREEN_WIDTH; x++) {
-      float x_ratio = x * 8.0 / SCREEN_WIDTH;
-      float y_ratio = y * 8.0 / SCREEN_HEIGHT;
-      int x_int = (int)x_ratio;
-      int y_int = (int)y_ratio;
-      float x_frac = x_ratio - x_int;
-      float y_frac = y_ratio - y_int;
-
-      int index_tl = y_int * 8 + x_int;
-      int index_tr = y_int * 8 + (x_int < 7 ? x_int + 1 : x_int);
-      int index_bl = (y_int < 7 ? y_int + 1 : y_int) * 8 + x_int;
-      int index_br = (y_int < 7 ? y_int + 1 : y_int) * 8 + (x_int < 7 ? x_int + 1 : x_int);
-
-      float interp_val = pixels_raw[index_tl] * (1 - x_frac) * (1 - y_frac) +
-                         pixels_raw[index_tr] * x_frac * (1 - y_frac) +
-                         pixels_raw[index_bl] * (1 - x_frac) * y_frac +
-                         pixels_raw[index_br] * x_frac * y_frac;
-
-      // Map temperature value to color
-      uint16_t color = temperatureToColor(interp_val);
-      tft.drawPixel(x, y, color);
+    amg.readPixels(pixels); // Odczyt danych z czujnika
+    
+    // Interpolacja danych
+    interpolatePixels();
+    
+    // Mapowanie temperatur na kolory
+    for (int y = 0; y < 30; y++) {
+        for (int x = 0; x < 40; x++) {
+            colors[x][y] = mapTemperatureToColor(interpolatedPixels[x][y]);
+        }
     }
-  }
-  Serial.println("Interpolation and display update complete");
-
-  // Display the central temperature value
-  float central_temp = pixels_raw[4 * 8 + 4];  // Temperature at the central point (4,4)
-  tft.setCursor(10, 10);
-  tft.fillRect(0, 0, 320, 30, ILI9341_BLACK);  // Clear the area where text will be displayed
-  tft.print("Center Temp: ");
-  tft.print(central_temp, 2);
-  tft.print(" C");
-  Serial.print("Central Temp: ");
-  Serial.print(central_temp, 2);
-  Serial.println(" C");
-
-  delay(100);  // Adjust delay as needed for display rate
-}
-
-uint16_t temperatureToColor(float temp) {
-  uint8_t r, g, b;
-  if (temp < 20) {
-    r = 0;
-    g = 0;
-    b = map(temp, 0, 20, 255, 0);
-  } else if (temp < 30) {
-    r = 0;
-    g = map(temp, 20, 30, 0, 255);
-    b = 0;
-  } else {
-    r = map(temp, 30, 40, 0, 255);
-    g = 255 - r;
-    b = 0;
-  }
-  return tft.color565(r, g, b);
+    
+    // Wyświetlanie danych na ekranie
+    for (int y = 0; y < 30; y++) {
+        for (int x = 0; x < 40; x++) {
+            tft.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight, colors[x][y]);
+        }
+    }
+    
+    // Wyświetlanie skali temperatur
+    int scaleHeight = 20;
+    for (int i = 0; i < displayWidth; i++) {
+        float temp = 20.0 + (i * (40.0 - 20.0) / displayWidth);
+        uint16_t color = mapTemperatureToColor(temp);
+        tft.drawFastVLine(i, displayHeight - scaleHeight, scaleHeight, color);
+    }
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setCursor(10, displayHeight - scaleHeight - 10); // Pozycja dla 20°C
+    tft.print("20C");
+    tft.setCursor(displayWidth - 30, displayHeight - scaleHeight - 10); // Pozycja dla 40°C
+    tft.print("40C");
+    
+    // Wyświetlanie temperatury centralnego punktu
+    float centralTemp = pixels[27]; // Centralny punkt matrycy 8x8
+    tft.setCursor(displayWidth / 2 - 20, displayHeight / 2 - 10); // Pozycja centralna
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+    tft.setTextSize(2);
+    tft.print(centralTemp, 1);
+    tft.print("C");
+    
+    delay(500); // Czekanie na kolejny odczyt (czas można dostosować)
 }
